1c7f45597240cd5603f428f78f297aa60766fb9c
kota-yamashita <mcz-0604-htt@outlook.com>
2019-10-20 02:17:36 +0900

    add Trade-API

--- api/src/app/Http/Controllers/Api/TradesController.php
+++ api/src/app/Http/Controllers/Api/TradesController.php
@@ -8,79 +8,118 @@ use App\Http\Controllers\Controller;
 
 class TradesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $trade = new Trade();
+
+        $trade->sneaker_id = $request->input('sneakerId');
+        $trade->size = $request->input('size');
+        $trade->price = $request->input('tradePrice');
+        $trade->trade_date = $request->input('tradeDate');
+
+        $trade->save();
+
+        return response([], 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $trade = new Trade();
+
+        $data = $trade->getData($id);
+
+        $data_json = [
+            'id' => $id,
+            'sneakerNum' => count($data),
+            'sneakers' => self::toArray($data),
+        ];
+
+        return response()->json(
+            $data_json,
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function showSize($id)
     {
-        //
+        $trade = new Trade();
+
+        $data = $trade->getSizeData($id);
+
+        $data_json = [
+            'id' => $id,
+            'sizes' => self::toArraySize($data),
+        ];
+
+        return response()->json(
+            $data_json,
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    private function toArray($requet_obejct)
     {
-        //
+        $response_arr = [];
+
+        $i = 0;
+        while(count($requet_obejct) > $i){
+            $arr_data  = [
+                'id' => $requet_obejct[$i]->id,
+                'size' => $requet_obejct[$i]->size,
+                'tradePrice' => $requet_obejct[$i]->tradePrice,
+                'tradeDate' => $requet_obejct[$i]->tradeDate,
+            ];
+            $response_arr[] = $arr_data;
+
+            ++$i;
+        }
+
+        return $response_arr;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    private function toArraySize($requet_obejct)
     {
-        //
+        $response_arr = [];
+
+        $sizeFigure = [0, 25, 25.5, 26, 26.5, 27, 27.5, 28, 28.5, 29, 29.5, 30];
+
+        foreach($sizeFigure as $size){
+            $arr_data = [
+                'size' => $size,
+                'lowestPrice' => self::getPriveFromSize($size, $requet_obejct)
+            ];
+            $response_arr[] = $arr_data;
+        }
+
+        return $response_arr;
+    }
+
+    private function getPriveFromSize($size, $requet_obejct)
+    {
+        if($size == 0){
+            $lowestPrice = $requet_obejct[0]->lowestPrice;
+            $i = 1;
+            while(count($requet_obejct) > $i){
+                if($lowestPrice > $requet_obejct[$i]->lowestPrice){
+                    $lowestPrice = $requet_obejct[$i]->lowestPrice;
+                }
+                ++$i;
+            }
+            return $lowestPrice;
+        }
+
+        $i = 0;
+        while(count($requet_obejct) > $i){
+            if($size == $requet_obejct[$i]->size){
+                return $requet_obejct[$i]->lowestPrice;
+            }
+            ++$i;
+        }
+
+        return 0;
     }
 }
--- api/src/app/Models/Trade.php
+++ api/src/app/Models/Trade.php
@@ -7,10 +7,49 @@ use Illuminate\Support\Facades\DB;
 
 class Trade extends Model
 {
-    {
-        protected $table = 'trades';
+    protected $table = 'trades';
+
+    protected $guarded = array('id');
     
-        protected $guarded = array('id');
-      
-        public $timestamps = true;
+    public $timestamps = true;
+
+    protected $fillable =[
+        'sneaker_id'
+        , 'size'
+        , 'price'
+        , 'trade_date'
+    ];
+
+    public function getData($id=null)
+    {
+      $query = DB::table($this->table);
+
+      if($id != null) $query->where('sneaker_id', $id);
+    
+      $data = $query->select(
+                        'id'
+                        , 'size'
+                        , 'price as tradePrice'
+                        , 'trade_date as tradeDate'
+                        )
+                    ->get();
+
+      return $data;
+    }
+
+    public function getSizeData($id=null)
+    {
+      $query = DB::table($this->table);
+
+      if($id != null) $query->where('sneaker_id', $id);
+    
+      $data = $query->select(
+                        \DB::raw('min(price) as lowestPrice,
+                        size'
+                    ))
+                    ->groupBy(\DB::raw('size'))
+                    ->get();
+
+      return $data;
+    }
 }
