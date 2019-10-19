eb87caebb77495fb716c273a6f9771598a066652
HallelujahStone <aaharuyana@gmail.com>
2019-10-19 14:36:07 +0900

    Add 3D-Viewer, add basic gesture

--- app/LookerETASTE/ViewControllers/ItemViewController.swift
+++ app/LookerETASTE/ViewControllers/ItemViewController.swift
@@ -8,11 +8,14 @@
 
 import Foundation
 import UIKit
+import SceneKit
 
 class ItemViewController: UIViewController{
     
     @IBOutlet weak var buyButton: UILabel!
     @IBOutlet weak var sealButton: UILabel!
+    
+    var node: SCNNode!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,40 @@ class ItemViewController: UIViewController{
         
         self.title = "Item"
         
+        initializeSceneView()
+        
     }
     
+    func initializeSceneView() {
+        let nib = UINib(nibName: "Viewer3D", bundle: nil)
+        let viewer3D = nib.instantiate(withOwner: nil, options: nil)[0] as! Viewer3D
+        let scene = SCNScene(named: "SceneKit Asset Catalog.scnassets/300/shoes_one copy.scn")
+        viewer3D.sceneView.scene = scene
+        viewer3D.sceneView.autoenablesDefaultLighting = true
+        node = (scene!.rootNode.childNode(withName: "shoes1", recursively: false))!
+        node.position = SCNVector3(0,0,0)
+        viewer3D.sceneView.scene!.rootNode.addChildNode(node)
+        // ビュー背景透過
+        viewer3D.backgroundColor = UIColor.clear
+        viewer3D.sceneView.backgroundColor = UIColor.clear
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.panViewer3D(sender:)))
+        viewer3D.addGestureRecognizer(panGestureRecognizer)
+        
+        viewer3D.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2) // とりあえず中心に
+        self.view.addSubview(viewer3D)
+    }
+    
+    @objc func panViewer3D(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            let deviation: CGPoint = sender.translation(in: view)
+            let rotation: CGFloat =  (CGFloat(Float.pi / 180) / 2) // オイラー角
+            node.eulerAngles = SCNVector3(CGFloat(node.eulerAngles.x) + deviation.y * rotation, CGFloat(node.eulerAngles.y) + deviation.x * rotation, 0)
+            sender.setTranslation(CGPoint(x: 0, y: 0), in: view)
+        case .ended:
+            break
+        default:
+            break
+        }
+    }
 }
---
+++ app/LookerETASTE/Viewer3D.swift
@@ -0,0 +1,16 @@
+//
+//  Viewer3D.swift
+//  LookerETASTE
+//
+//  Created by 石黒晴也 on 2019/10/19.
+//  Copyright © 2019 looker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SceneKit
+
+class Viewer3D: UIView {
+    
+    @IBOutlet weak var sceneView: SCNView!
+}
